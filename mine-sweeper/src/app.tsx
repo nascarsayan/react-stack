97012411798c6f4bba1907989aec778f4dc9e519
Sayan Naskar <nascarsayan@gmail.com>
2024-01-04 10:12:17 +0530

    generate mines, create board

--- mine-sweeper/src/app.tsx
+++ mine-sweeper/src/app.tsx
@@ -1,33 +1,72 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'preact/hooks';
+
 import './app.css'
 
+type Cell = {
+  isMine: boolean
+}
+
 export function App() {
-  const [count, setCount] = useState(0)
+
+  const [board, setBoard] = useState<Array<Array<Cell>>>([])
+
+  let numRows = 8
+  let numCols = 10
+
+  function getMineCells() {
+    let totalMines = 8 // (numRows * numCols) / 6
+
+    let allCells = (new Array(numRows * numCols)).fill(0).map(
+      (_, i) => i
+    )
+
+    // shuffle the cells
+
+    for (let i = allCells.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [allCells[i], allCells[j]] = [allCells[j], allCells[i]];
+    }
+
+    let mineCells = allCells.slice(0, totalMines)
+
+    return mineCells
+  }
+
+  function createBoard() {
+    let mineCells = getMineCells()
+
+    let newBoard: Array<Array<Cell>> = []
+    for (let i = 0; i < numRows; i++) {
+
+      let currRow: Array<Cell> = []
+      for (let j = 0; j < numCols; j++) {
+        let isMine = mineCells.includes(i * numCols + j)
+        currRow.push({ isMine })
+      }
+
+      newBoard.push(currRow)
+    }
+
+    setBoard(newBoard)
+  }
+
+  function getRow() {
+    return (
+      <div className="row">
+        {
+          new Array(numCols).fill(0).map(
+            _ => <div className="cell"> * </div>
+          )
+        }
+      </div>
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <div id="app">
+      {
+        new Array(numRows).fill(0).map(_ => getRow())
+      }
+    </div>
   )
 }
